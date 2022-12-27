5cdcfe2857af341291b21fc0ec1c4cf7ee40d779
Wissididom <webmaster@wissididom.me>
2022-12-27 23:40:45 +0100

    Aeded script tags to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Twitch-Discord-Message-Manager</title>
+        <script type="application/javascript" src="lib/tmi.min.js"></script>
+        <script defer type="application/javascript" src="app.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
